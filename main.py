eaff3f73e5def5a72770b23ecf65229845b38fd7
w1png <w1png@yandex.ru>
2022-08-23 16:09:31 +0300

    Add threading

--- main.py
+++ main.py
@@ -3,25 +3,31 @@ from string import digits, ascii_lowercase
 import requests, fake_headers
 from os.path import getsize
 from os import remove
+import threading
 
 
 BASE_URL = "https://i.imgur.com/"
 CHARACTERS = digits + ascii_lowercase
 IMAGE_EXTENTIONS = [".webp", ".png", ".jpg"]
 IMAGE_COUNTER = 0
+THREADS = 12
 
 
 def generate_urlId() -> str:
     return "".join([choice(CHARACTERS) for _ in range(6)]) + choice(IMAGE_EXTENTIONS)
 
 
-def download(urlId: str) -> None:
+def download() -> None:
+    print("Starting download!")
+    urlId = generate_urlId()
+    url = BASE_URL + urlId
     filename = "images/" + urlId
 
     with open(filename, "wb") as f:
-        f.write(requests.get(BASE_URL + urlId, headers=fake_headers.make_header()).content)
+        f.write(requests.get(url, headers=fake_headers.make_header()).content)
 
     if getsize(filename) < 10000:
+        print(f"Download failed for {urlId}")
         return remove(filename)
    
     global IMAGE_COUNTER
@@ -30,5 +36,12 @@ def download(urlId: str) -> None:
 
 
 while True:
-    download(generate_urlId())
+    threads = [threading.Thread(target=download) for _ in range(THREADS)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    
+
+
 
